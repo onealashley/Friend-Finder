87c88a383bb124c42b191ff28326530aa07f7c12
ashley o <oneal.ashleyc@gmail.com>
2018-08-08 15:43:42 -0400

    final

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendList = [
+    {
+      name: 'Jerry',
+      profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKh3ghyOK08jzHgNBmlaz6r3gLfjoSBx7rEfk_4qGQTq2uf2v9Gg',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    },
+    {
+        name: 'Harry',
+      profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9qO57VEvAxRlDgbQA1JCWZ3JjEebQ9mEqMHEnodLOTZap2fMFwQ',
+      scores: [
+        "1",
+        "3",
+        "4",
+        "5",
+        "2",
+        "4",
+        "5",
+        "3",
+        "2",
+        "1"
+      ]
+    },
+    {
+        name: 'George',
+      profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4b6elrr-uSkPxN0wW3C65_0yopg9Cu0zIzeTWUfTMRtwfgxI0',
+      scores: [
+        "4",
+        "2",
+        "2",
+        "4",
+        "5",
+        "1",
+        "1",
+        "3",
+        "2",
+        "5"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-Parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("listening on port: " + PORT);
+});
